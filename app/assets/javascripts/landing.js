fb228930d95fd507bb6bfa53e5fa282b5708e86c
RaSiel <bloodyperson@mail.ru>
2016-02-08 13:53:48 +0300

    Import base templates

---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,10 @@
+jQuery.ias({
+         container :  ".ajscroll",
+         item: ".item",
+         pagination: ".pagination",
+         next: ".pagination-next a",
+         triggerPageThreshold: "99999",
+trigger: "load more items",
+         loader: "<img src=\"/media/ajax_scroll/assets/loader.gif\"/>",
+         noneleft: "<i class=\"Jext_more\">no more</i>"
+  });
