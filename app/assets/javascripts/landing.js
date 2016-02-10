6fc5c0f102fd2757c00df5c299105f4d12033123
RaSiel <bloodyperson@mail.ru>
2016-02-10 13:36:45 +0300

    Corrected imported styles

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,10 +1,11 @@
 jQuery.ias({
-         container :  ".ajscroll",
-         item: ".item",
-         pagination: ".pagination",
-         next: ".pagination-next a",
-         triggerPageThreshold: "99999",
-trigger: "load more items",
-         loader: "<img src=\"/media/ajax_scroll/assets/loader.gif\"/>",
-         noneleft: "<i class=\"Jext_more\">no more</i>"
-  });
+  container :  ".carouselck_images",
+  item: "img",
+  pagination: ".pagination",
+  next: ".carouselck_next",
+  prev: ".carouselck_prev",
+  triggerPageThreshold: "99999",
+  trigger: "load more items",
+  loader: "<img src=\"/media/ajax_scroll/assets/loader.gif\"/>",
+  noneleft: "<i class=\"Jext_more\">no more</i>"
+});
